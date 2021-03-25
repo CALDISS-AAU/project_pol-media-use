587ac32be7ddefb9a1279fee89e6a7bc51a73e9a
Kristian Gade Kjelmann <kgk@adm.aau.dk>
2021-03-25 13:47:18 +0100

    fix indent

--- scripts/start_watching_cron.py
+++ scripts/start_watching_cron.py
@@ -22,8 +22,8 @@ from email.mime.text import MIMEText
 logger = logging.getLogger(__name__)
 
 def main():
-	
-	#Setting up counter
+    
+    #Setting up counter
     if os.path.isfile(os.path.join(logdir, 'article_counter.json')):
         with open(os.path.join(logdir, 'article_counter.json'), 'r') as f:
             ARTICLE_COUNTER = json.load(f)
@@ -34,59 +34,59 @@ def main():
                        "TV2": 0,
                        "EB": 0,
                        "JP": 0}
-	
-	#Parameters for watch
-	keywords = [r".*"]
+    
+    #Parameters for watch
+    keywords = [r".*"]
 
-	sources = ["DR", "Politiken", "Berlingske", "TV2", "EB", "JP"]
-	
-	dt_now = datetime.now()
+    sources = ["DR", "Politiken", "Berlingske", "TV2", "EB", "JP"]
+    
+    dt_now = datetime.now()
 
-	#Setting up e-mail notifications
+    #Setting up e-mail notifications
 
-	msg = MIMEMultipart()
+    msg = MIMEMultipart()
 
-	msg['From'] = 'kristian@mail.bot'
-	msg['To'] = 'kgk@adm.aau.dk'
-	msg['Subject'] = "Error running headline_watcher"
+    msg['From'] = 'kristian@mail.bot'
+    msg['To'] = 'kgk@adm.aau.dk'
+    msg['Subject'] = "Error running headline_watcher"
 
-	s = smtplib.SMTP('smtp-relay.sendinblue.com', 587)
-	s.ehlo()
-	s.starttls()
-	s.ehlo()
-	s.login("kgk@adm.aau.dk", "a94Q187jgzb0vWEO")
-	
-	for source in sources:
-		print("{time}: Checking {source} front page...".format(time = dt_now, source = source))
-		logger.info("Checking {source} front page...".format(source = source))
-		try:
-			count = fp_watcher.headline_watch(source = source, keywords = keywords, datadir = datadir)
-		except Exception as e:
-			logger.error("Failed to run watch on {source}: ".format(source = source) + str(e))
-			message = "<p><i>Watch on {source} was halted with the following error:</i> <br /> <br /> {error}</p>".format(source = source, error = e)
-			msg.attach(MIMEText(message, 'html'))
+    s = smtplib.SMTP('smtp-relay.sendinblue.com', 587)
+    s.ehlo()
+    s.starttls()
+    s.ehlo()
+    s.login("kgk@adm.aau.dk", "a94Q187jgzb0vWEO")
+    
+    for source in sources:
+        print("{time}: Checking {source} front page...".format(time = dt_now, source = source))
+        logger.info("Checking {source} front page...".format(source = source))
+        try:
+            count = fp_watcher.headline_watch(source = source, keywords = keywords, datadir = datadir)
+        except Exception as e:
+            logger.error("Failed to run watch on {source}: ".format(source = source) + str(e))
+            message = "<p><i>Watch on {source} was halted with the following error:</i> <br /> <br /> {error}</p>".format(source = source, error = e)
+            msg.attach(MIMEText(message, 'html'))
 
-			s.send_message(msg)   
-		
-		if count == 0:
-			ARTICLE_COUNTER[source] = ARTICLE_COUNTER[source] + 1
-		else:
-			ARTICLE_COUNTER[source] = 0
-		
-		if ARTICLE_COUNTER[source] >= 36:
-			message = "<p><i>Warning: Watch on {source} has run {n} times with 0 new articles found</i></p>.".format(source = source, n = ARTICLE_COUNTER[source])
-			msg.attach(MIMEText(message, 'html'))
-			
-			s.send_message(msg)
-		
-		with open(os.path.join(logdir, 'article_counter.json', 'w')) as f:
+            s.send_message(msg)   
+        
+        if count == 0:
+            ARTICLE_COUNTER[source] = ARTICLE_COUNTER[source] + 1
+        else:
+            ARTICLE_COUNTER[source] = 0
+        
+        if ARTICLE_COUNTER[source] >= 36:
+            message = "<p><i>Warning: Watch on {source} has run {n} times with 0 new articles found</i></p>.".format(source = source, n = ARTICLE_COUNTER[source])
+            msg.attach(MIMEText(message, 'html'))
+            
+            s.send_message(msg)
+        
+        with open(os.path.join(logdir, 'article_counter.json', 'w')) as f:
             json.dump(ARTICLE_COUNTER, f)
         
-#	s.quit()
-	
+#    s.quit()
+    
 
 if __name__ == '__main__':
-	FORMAT = '%(asctime)-15s %(message)s'
-	logout = os.path.join(logdir, 'watcher.log')
-	logging.basicConfig(filename=logout, filemode='a', level=logging.INFO, format = FORMAT)
-	main()
+    FORMAT = '%(asctime)-15s %(message)s'
+    logout = os.path.join(logdir, 'watcher.log')
+    logging.basicConfig(filename=logout, filemode='a', level=logging.INFO, format = FORMAT)
+    main()
